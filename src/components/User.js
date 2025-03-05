0c2245386e05e23ba50b5636ffaf4db268c09ca1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-05 14:37:13 -0500

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
 function User(){
+
+    let[user,setUser]=useState();
+
+    async function getAllUsers(){
+        let response=await fetch('https://dummyjson.com/users');
+        let result=await response.json();
+        setUser(result.users);
+
+    }
     return(
         <>
+        <h3>User Details</h3>
+        <table border='2'>
+            <tr>
+                <th>Id</th>
+                <th>FirstName</th>
+                <th>LastName</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>BloodGroup</th>
+            </tr>
+            {
+                user.map((x)=>{
+                    return(
+                        <tr key={x.id}>
+                           
+
+                        </tr>
+                    )
+                })
+            }
+        </table>
         </>
     );
 }
