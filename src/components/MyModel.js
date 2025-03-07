ab0a53ecb65fec8cf325effc1ebb8063f3d59995
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-07 14:30:18 -0500

    Update MyModel.js

--- src/components/MyModel.js
+++ src/components/MyModel.js
@@ -1,71 +1,72 @@
-import { Button, Modal } from "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Modal } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
-function MyModel(props){
-    let[modelOpen,setModelOpen]=useState(false);
+function MyModel(props) {
+    let [modelOpen, setModelOpen] = useState(false);
 
-    let[id,setId]=useState();
-    let[firstName,setFirstName]=useState();
-    let[lastName,setlastName]=useState();
-    let[age,setAge]=useState();
-    let[gender,setGender]=useState();
+    let [id, setId] = useState('');
+    let [firstName, setFirstName] = useState('');
+    let [lastName, setLastName] = useState('');
+    let [age, setAge] = useState('');
+    let [gender, setGender] = useState('');
 
+    function closeModel() {
+        setModelOpen(false);
+    }
 
+    useEffect(() => {
+        if (props.rowData) {
+            setModelOpen(true);
+            setId(props.rowData.id);
+            setFirstName(props.rowData.firstName);
+            setLastName(props.rowData.lastName);
+            setAge(props.rowData.age);
+            setGender(props.rowData.gender);
+        }
+    }, [props.rowData]);
 
+    function funUpdate() {
+        // Update functionality
+    }
 
-useEffect(()=>{
-    setModelOpen(true);
-    setId(props.rowdata.id);
-    setFirstName(props.rowdata.firstName);
-    setlastName(props.rowdata.lastName);
-    setAge(props.rowdata.age);
-    setGender(props.rowdata.gender);
-    
-})
+    function funDelete() {
+        // Delete functionality
+    }
 
-function funUpdate(){
-
-}
-
-function funDelete(){
-    
-}
-
-    return(
+    return (
         <>
-        <Modal>
-            <Modal.Dialog>
-                <Modal.Header>
-                    <Modal.Title>
-                        User Details
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div>
-                        Id:<input type="text" defaultValue={id} ></input>
-                    </div>
-                    <div>
-                        FirstName:<input type="text" defaultValue={firstName}></input>
-
-                    </div>
-                    <div>
-                        LastName:<input type="text" defaultValue={lastName}></input>
-
-                    </div>
-                    <div>
-                        Age:<input type="text" defaultValue={age}></input>
-                    </div>
-                    <div>
-                        Gender:<input type="text" defaultValue={gender}></input>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onclick={closeModel}></Button>
-                    <Button variant="secondary" onclick={funUpdate}></Button>
-                    <Button variant="secondary" onclick={funDelete}></Button>
-                </Modal.Footer>
-            </Modal.Dialog>
-        </Modal>
+            <Modal show={modelOpen} onHide={closeModel}>
+                <Modal.Dialog>
+                    <Modal.Header>
+                        <Modal.Title>
+                            User Details
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>
+                            Id:<input type="text" defaultValue={id}></input>
+                        </div>
+                        <div>
+                            FirstName:<input type="text" defaultValue={firstName}></input>
+                        </div>
+                        <div>
+                            LastName:<input type="text" defaultValue={lastName}></input>
+                        </div>
+                        <div>
+                            Age:<input type="text" defaultValue={age}></input>
+                        </div>
+                        <div>
+                            Gender:<input type="text" defaultValue={gender}></input>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeModel}>Close</Button>
+                        <Button variant="primary" onClick={funUpdate}>Update</Button>
+                        <Button variant="danger" onClick={funDelete}>Delete</Button>
+                    </Modal.Footer>
+                </Modal.Dialog>
+            </Modal>
         </>
     );
 }
