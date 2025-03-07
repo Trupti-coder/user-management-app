23b807c048eb1f4675f08da3e43dd4df8413b492
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-06 23:04:36 -0500

    Update MyModel.js

--- src/components/MyModel.js
+++ src/components/MyModel.js
@@ -1,6 +1,19 @@
-function MyModel(){
+import { Modal } from "bootstrap";
+import { useState } from "react";
+
+function MyModel(props){
+    let[modelOpen,setModelOpen]=useState(false);
     return(
         <>
+        <Modal>
+            <Modal.Dialog>
+                <Modal.Header>
+                    <Modal.Title>
+                        User Details
+                    </Modal.Title>
+                </Modal.Header>
+            </Modal.Dialog>
+        </Modal>
         </>
     );
 }
