e0df1353d76a1d465f4a1d3ad65f67cf981e0d1a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-07 21:59:05 -0500

    Update AddNewUser.js

--- src/components/AddNewUser.js
+++ src/components/AddNewUser.js
@@ -1,1 +1,24 @@
+function AddNewUser(){
+    return(
+        <>
+        <form>
+            Id:<input type="text"
+            value={id}
+            placeholder="Enter Id"
+            />
 
+            FirstName:<input type="text"
+            value={firstname}
+            placeholder="Enter FirstName"
+            />
+
+            LastName:<input type="text"
+            value={lastname}
+            placeholder="Enter LastName"
+            />
+        </form>
+
+        </>
+    );
+}
+export default AddNewUser;
