37dcdb072cb262c67a3e48541ab77662d9b77d7d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-07 22:12:56 -0500

    Update AddNewUser.js

--- src/components/AddNewUser.js
+++ src/components/AddNewUser.js
@@ -20,31 +20,37 @@ function AddNewUser(){
             Id:<input type="text"
             value={id}
             placeholder="Enter Id"
+            onChange={(event)=>setId(event.target.value)}
             />
 
             FirstName:<input type="text"
             value={firstname}
             placeholder="Enter FirstName"
+            onChange={(event)=>setFirstName(event.target.value)}
             />
 
             LastName:<input type="text"
             value={lastname}
             placeholder="Enter LastName"
+            onChange={(event)=>setLastName(event.target.value)}
             />
 
             Age:<input type="text"
             value={age}
             placeholder="Enter Age"
+            onChange={(event)=>setAge(event.target.value)}
             />
 
             Gender:<input type="text"
             value={gender}
             placeholder="Enter Gender"
+            onChange={(event)=>setGender(event.target.value)}
             />
 
             BloodGroup:<input type="text"
             value={bloodgroup}
             placeholder="Enter BloodGroup"
+            onChange={(event)=>setBloodGroup(event.target.value)}
             />
             <button type="submit">Add User</button>
         </form>
