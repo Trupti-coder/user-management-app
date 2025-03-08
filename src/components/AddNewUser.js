6a8224ef7add959d5a17fda1c65d3b153631c6d6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-07 22:02:17 -0500

    Update AddNewUser.js

--- src/components/AddNewUser.js
+++ src/components/AddNewUser.js
@@ -16,6 +16,21 @@ function AddNewUser(){
             value={lastname}
             placeholder="Enter LastName"
             />
+
+            Age:<input type="text"
+            value={age}
+            placeholder="Enter Age"
+            />
+
+            Gender:<input type="text"
+            value={gender}
+            placeholder="Enter Gender"
+            />
+
+            BloodGroup:<input type="text"
+            value={bloodgroup}
+            placeholder="Enter BloodGroup"
+            />
         </form>
 
         </>
